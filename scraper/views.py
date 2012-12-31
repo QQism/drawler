03721586e0613a72de38793745f07b4a0a2c9655
Quang Quach <me@quang.be>
2012-12-31 19:06:30 +0700

    handle basic HTTP auth with simple API

--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,11 @@
+from functools import wraps
+
+from django.core import serializers
 from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from models import ScraperProfile, ScraperSession
 from forms import ScraperSessionForm, ScraperProfileForm
@@ -12,6 +16,22 @@ import json
 import requests
 logger = logging.getLogger(__name__)
 
+def http_basic_auth(func):
+    @wraps(func)
+    def _decorator(request, *args, **kwargs):
+        from django.contrib.auth import authenticate, login
+        if request.META.has_key('HTTP_AUTHORIZATION'):
+            authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+            if authmeth.lower() == 'basic':
+                auth = auth.strip().decode('base64')
+                username, password = auth.split(':', 1)
+                user = authenticate(username=username, password=password)
+                if user:
+                    login(request, user)
+        return func(request, *args, **kwargs)
+    return _decorator
+
+@http_basic_auth
 @login_required
 def home(request):
     """List out site profiles
@@ -22,7 +42,7 @@ def home(request):
                               {'profiles': profiles},
                               context_instance=RequestContext(request))
 
-
+@http_basic_auth
 @login_required
 def new_profile(request):
     if request.method == 'GET':
@@ -44,7 +64,7 @@ def new_profile(request):
             {'form': form},
             context_instance=RequestContext(request))
 
-
+@http_basic_auth
 @login_required
 def profile(request, profile_id):
     """Select the profile
@@ -63,30 +83,48 @@ def profile(request, profile_id):
                               },
                               context_instance=RequestContext(request))
 
+@csrf_exempt
+@http_basic_auth
+#@login_required
 def sessions(request, profile_id):
     """
     """
     profile = ScraperProfile.objects.get(pk=profile_id)
     form = ScraperSessionForm()
     logger.info(request)
+    print request
     if request.method == 'POST':
-        form = ScraperSessionForm(request.POST)
-        if form.is_valid():
+        if request.META['HTTP_ACCEPT'] == 'application/json':
+            items = request.POST.items()
+            post = json.loads(items[0][0])
+
             new_session = ScraperSession(profile=profile,
-                                         max_nodes=int(form.data['max_pages']),
-                                         max_added_nodes=int(form.data['pages_increment']),
-                                         timeout=int(form.data['timeout']))
+                                         max_nodes=int(post['max_pages']),
+                                         max_added_nodes=int(post['pages_increment']),
+                                         callback_url=post['callback_url'],
+                                         timeout=int(post['timeout']))
             new_session.save()
-            #enqueue(scrape, session_id=new_session.pk)
-            #scrape.delay(new_session.id)
-            #_opic.start()
-            return redirect('scraper:session', profile_id=profile.id,
-                            session_id=new_session.id)
+            data = {'message': 'Success'}
+
+            return HttpResponse(json.dumps(data), mimetype='application/json')
         else:
-            return render_to_response('scraper/profile.html',
-                                      {'profile': profile,
-                                       'form': form},
-                                      context_instance=RequestContext(request))
+            form = ScraperSessionForm(request.POST)
+            if form.is_valid():
+                new_session = ScraperSession(profile=profile,
+                                             max_nodes=int(form.data['max_pages']),
+                                             max_added_nodes=int(form.data['pages_increment']),
+                                             timeout=int(form.data['timeout']))
+                new_session.save()
+                #enqueue(scrape, session_id=new_session.pk)
+                #scrape.delay(new_session.id)
+                #_opic.start()
+                return redirect('scraper:session', profile_id=profile.id,
+                                session_id=new_session.id)
+            else:
+                return render_to_response('scraper/profile.html',
+                                          {'profile': profile,
+                                           'form': form},
+                                          context_instance=RequestContext(request))
 
 def session(request, profile_id, session_id):
     session = ScraperSession.objects.get(pk=session_id)
