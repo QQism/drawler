c0cf63f4678045a1211a3bf74e7d932beb8ec9ef
Quang Quach <me@quang.be>
2013-01-02 07:09:19 +0700

    update increment crawling

--- scraper/increment.py
+++ scraper/increment.py
@@ -3,18 +3,45 @@ from . import Crawler, TemplateProcessor
 def build_url(head, tail, step):
     return head + str(step) + tail
 
+def initial_values(node):
+    node['cash'] = 0
+    node['history'] = 0.0
+    node['keywords_count'] = 0
+    node['importance'] = 0
+    node['kopic'] = 0
+    node.setdefault('cache', [])
+    return node
+
 def start(head, tail, min_id=0, max_id=0, template='', max_nodes=20, writer=None, cache=None):
     crawler = Crawler()
     tp = TemplateProcessor(tp)
     nodes = []
+    node_count = 0
     for step in range(min_id, max_id+1):
+        # check if maximum exeededs
+        if node_count >= max_nodes:
+            break
+        else:
+            node_count += 1
+
         # initial node
         url = build_url(head, tail, step)
-        node = {'name': url}
+        node = initial_values({'name': url})
+
         # crawling
         response = crawler.goto(url)
+        node['raw_content'] = response
+
         # scraping, extraction
-        tp.extract(response.content)
-        
+        content = tp.extract(response.content)
+
+        # append into the list
+        nodes.append(node)
+
+        # calculate importance
+
+
         # saving
+        if writer is not None:
+            writer(node)
     return
